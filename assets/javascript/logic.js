d426ac0e77791447c7ffcb3e04fa665796e12383
Drake Smith <drakesmithjr@gmail.com>
2016-08-26 02:36:38 -0700

    test

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -44,7 +44,7 @@ $(".text_process_button").click(function(){//This is the Get Sentiment Scores bu
             //$("#sentimentScorePanel").html(result.type+" = "+result.score);
             var scorePercent = Math.round(Math.abs(result.score * 100));
             // $('#sentiment').html(result.type);
-            $('#sentscore').html(scorePercent + "%" + " " result.type + ".");
+            $('#sentscore').html(scorePercent + "%" + " " + result.type + ".");
 
             // $("#sentimentScore").html(result.score);
             //The Math.abs() function returns the absolute value of a number
