d99b286d37e4a6a16dd0990cf27fa55d8970fa20
Drake Smith <drakesmithjr@gmail.com>
2016-08-26 03:00:26 -0700

    test

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -96,7 +96,7 @@ $(".text_process_button").click(function(){//This is the Get Sentiment Scores bu
             var capital = newWord.charAt(0).toUpperCase() + newWord.slice(1);
             fireset.push(capital);
             // $(".keywords > tbody").append("<tr><td>"+capital+"</td>" + "<td>"+(parseFloat(result.keywords[i].score.toFixed(4)))+"</td></tr>")
-            $(".keywords > tbody").append("<tr><td>" + results.keywords[i].word + "</td>" + "<td>" + results.keywords[i].score + "</td><tr>");
+            $(".keywords > tbody").append("<tr><td>" + result.keywords[i].word + "</td>" + "<td>" + result.keywords[i].score + "</td><tr>");
             allScores.push(parseFloat(result.keywords[i].score.toFixed(4)));
             }
 
